f243daed489194e11c8a74068f15da61fcaa5867
Ivan Silvestre <isilvestreneto@gmail.com>
2025-04-06 17:37:18 -0300

    feat: add Jest and JUnit icons, update skills marquee animation and hover effect

--- src/app/components/skills-marquee/skills-marquee.component.ts
+++ src/app/components/skills-marquee/skills-marquee.component.ts
@@ -9,13 +9,15 @@ import { Component } from '@angular/core';
 })
 export class SkillsMarqueeComponent {
   skills = [
-    { label: 'TypeScript', icon: 'icons/typescript.svg', color: '#3178C6' },
-    { label: 'Java', icon: 'icons/java.svg', color: '#ED8B00' },
     { label: 'Angular', icon: 'icons/angular.svg', color: '#DD0031' },
+    { label: 'TypeScript', icon: 'icons/typescript.svg', color: '#3178C6' },
+    { label: 'Jest', icon: 'icons/jest.svg', color: '#C21325' },
+    { label: 'Java', icon: 'icons/java.svg', color: '#ED8B00' },
+    { label: 'JUnit', icon: 'icons/junit5.svg', color: '#25A162' },
     { label: 'Spring', icon: 'icons/spring.svg', color: '#6DB33F' },
     { label: 'HTML', icon: 'icons/html.svg', color: '#E34F26' },
     { label: 'CSS', icon: 'icons/css.svg', color: '#1572B6' },
     { label: 'JavaScript', icon: 'icons/javascript.svg', color: '#F7DF1E' },
-    { label: 'SASS', icon: 'icons/sass.svg', color: '#CC6699' }
+    { label: 'SASS', icon: 'icons/sass.svg', color: '#CC6699' },
   ];
 }
